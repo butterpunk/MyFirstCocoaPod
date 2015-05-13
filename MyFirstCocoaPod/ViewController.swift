aa9b8debb9e72fa8a03431dd104e2df772d9fbc5
Blake Butterworth <blakebutterworth@blake.local>
2015-05-13 18:15:14 -0500

    Initial Commit

---
+++ MyFirstCocoaPod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstCocoaPod
+//
+//  Created by Blake Butterworth on 5/13/15.
+//  Copyright (c) 2015 Blake Butterworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
